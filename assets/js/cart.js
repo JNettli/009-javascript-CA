486ba799b66ccc820d42ccdb9f834c813f8996a0
Jonas Nettli <jonas.nettli@gmail.com>
2024-02-26 14:50:27 +0100

    Working deleting items from cart

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -10,6 +10,10 @@ window.onload = function() {
     
     cartCloseBtn.addEventListener('click', () => {
         cartBox.classList.remove('active');
+        function reload() {
+            location.reload();
+        }
+        setTimeout(reload, 220);
     });
     const iconShoppingP = document.querySelector('.addToCart p');
     let no = 0;
@@ -39,20 +43,19 @@ window.onload = function() {
         let updatedItems = items.filter(data => data.id !== itemId);
         localStorage.setItem('items', JSON.stringify(updatedItems));
         let tableData = '<tr><th>Item Name</th><th>Item No</th><th>Item Price</th></tr>';
-
-    if (!updatedItems || updatedItems.length === 0) {
-        tableData += '<tr><td colspan="4">There are no items in your cart!</td></tr>';
-        cartBox.classList.add('active');
-        window.location.reload();
-    } else {
-        updatedItems.forEach(data => {
-            tableData += `<tr><td>${data.name}</td><td>${data.no}</td><td>${data.price}</td><td><a href="#" onClick="Delete(${JSON.stringify(data)});">Delete</a></td></tr>`;
-            cartBox.classList.add('active');
-        });
+                
+        if (!updatedItems || updatedItems.length === 0) {
+            tableData += '<tr><td colspan="4">There are no items in your cart!</td></tr>';
+        } else {
+            updatedItems.forEach(data => {
+                tableData += `<tr id="item-${data.id}"><td>${data.name}</td><td>${data.no}</td><td>${data.price}</td><td><a href="#" onClick="Delete('${data.id}');">Delete</a></td></tr>`;
+                cartBox.classList.add('active');
+            });
+        }
+    
+        cardBoxTable.innerHTML = tableData;
     }
-
-    cardBoxTable.innerHTML = tableData;
-}
+    
 cardBoxTable.innerHTML = tableData;
 }
 
